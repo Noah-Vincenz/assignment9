7bffa8987ceaca54e413e859eb51ed9911fa7bfc
Noah-Vincenz Noeh <noah-vincenz.noeh@kcl.ac.uk>
2016-12-04 00:18:57 +0000

    first two functions finished

--- knights.h
+++ knights.h
@@ -17,11 +17,41 @@ pair<int,int> operator+(const pair<int,int> & a, const pair<int,int> & b) {
 }
 
 // TODO - your code goes here
+vector<pair<int, int> > moves(pair<int, int> pairIn) {
+    vector<pair<int, int> > vectorToReturn;
+    vectorToReturn.reserve(8);
+    vectorToReturn.push_back(make_pair(pairIn.first+1, pairIn.second-2));
+    vectorToReturn.push_back(make_pair(pairIn.first+2, pairIn.second-1));
+    vectorToReturn.push_back(make_pair(pairIn.first+2, pairIn.second+1));
+    vectorToReturn.push_back(make_pair(pairIn.first+1, pairIn.second+2));
+    vectorToReturn.push_back(make_pair(pairIn.first-1, pairIn.second+2));
+    vectorToReturn.push_back(make_pair(pairIn.first-2, pairIn.second+1));
+    vectorToReturn.push_back(make_pair(pairIn.first-2, pairIn.second-1));
+    vectorToReturn.push_back(make_pair(pairIn.first-1, pairIn.second-2));
+    return vectorToReturn;
+};
 
-
-
-
-
+vector<pair<int, int> > legal_moves(int dim, Path path, pair<int,int> pos) {
+    vector<pair<int, int> > movesVector = moves(pos);
+    vector<pair<int, int> > vectorToReturn;
+    std::copy_if(movesVector.begin(), movesVector.end(), back_inserter(vectorToReturn),
+                [dim, path](const pair<int, int> x) {
+                    return (x.first >= 0 && x.first < dim && x.second >= 0 && x.second < dim && find(path.begin(), path.end(), x) == path.end());
+                });
+    return vectorToReturn;
+};
+/*
+pair<Path,bool> first_tour (int dim, Path currentPath) {
+    if (currentPath.size() == dim * dim) {
+        return pair<Path,bool> (currentPath, true);
+    }
+    else {
+        for (pair <int, int> x : legal_moves(dim, currentPath, *currentPath.begin())) {
+            currentPath.push_back(x);
+            first_tour(dim, currentPath);
+        }
+    }
+}; */
 // Do not edit below this line
 
 #endif
