fa71e19503deb238dbb9745eec41c6d4def1f1a9
Noah-Vincenz Noeh <noah-vincenz.noeh@kcl.ac.uk>
2016-12-12 11:46:29 +0200

    Trade working

--- Trade.h
+++ Trade.h
@@ -1,14 +1,22 @@
 #ifndef TRADE_H
 #define TRADE_H
+#include <numeric>
+#include <utility>
+#include <iostream>
 
 // TODO your code goes here:
-
-
-
-
-
-
-
+template<class InputIt>
+int bestProfit(InputIt a, InputIt b) {
+    std::pair<int, int> currMax (*a, 0); //pair containing current min int as key and current max profit as value
+    std::accumulate(a, b, 0,
+                        [&currMax] (const int i, const int j) {
+                            int profit = j - currMax.first;
+                            if (profit > currMax.second) currMax.second = profit;
+                            if (j < currMax.first) currMax.first = j;
+                            return i;
+                        });
+    return currMax.second;
+}
 // Don't write any code below this line
 
 #endif
