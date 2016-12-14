a295ad9c591cd9b868c93085261e5c5b5d73b6b5
Noah-Vincenz Noeh <noah-vincenz.noeh@kcl.ac.uk>
2016-12-14 15:58:01 +0200

    Working

--- HowManyWaysToMake.h
+++ HowManyWaysToMake.h
@@ -1,8 +1,16 @@
 #ifndef HOW_MANY_WAYS_TO_MAKE_H
 #define HOW_MANY_WAYS_TO_MAKE_H
+#include <numeric>
+#include <utility>
 
 // TODO: Your code goes here
-
+template<class InputIt>
+int howManyWaysToMake (InputIt a, InputIt b, int amountOfMoney) {
+    if (amountOfMoney == 0) return 1;
+    else if (amountOfMoney < 0) return 0;
+    else if (a == b) return 0;
+    else return howManyWaysToMake(a, b, amountOfMoney - *a) + howManyWaysToMake(++a, b, amountOfMoney);
+}
 // Do not write any code below this line
 
 #endif
